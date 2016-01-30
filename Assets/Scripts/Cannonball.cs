39f45977b544dfd5d07b263f9f40ddd8bb39af33
moe-ped <m.berning@web.de>
2016-01-30 18:49:24 +0100

    Changed some stuff

--- Assets/Scripts/Cannonball.cs
+++ Assets/Scripts/Cannonball.cs
@@ -6,6 +6,7 @@ public class Cannonball : MonoBehaviour {
 	void OnCollisionEnter2D (Collision2D other) {
 		if (other.gameObject.tag == "Enemy") {
 			Destroy (other.gameObject);
+			Destroy (gameObject);
 		}
 	}
 }
---
+++ Assets/Scripts/Dragon/DragonAttack.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DragonAttack : MonoBehaviour {
+
+	// Use this for initialization
+	void OnTriggerEnter2D (Collider2D other) {
+		if (other.gameObject.tag == "Player") {
+			Destroy (gameObject);
+		}
+	}
+}
