af859ef3acb49ce4acde1882803ebf25b90c799b
moe-ped <m.berning@web.de>
2016-01-30 16:25:08 +0100

    Animated dragons

---
+++ Assets/Scripts/ShipHealth.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShipHealth : MonoBehaviour {
+
+	// Use this for initialization
+	void OnTriggerEnter2D (Collider2D other) {
+		Debug.Log ("ouch");
+		if (other.gameObject.tag == "Enemy") {
+			Destroy (other.gameObject);
+		}
+	}
+}
