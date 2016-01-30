92fa5dfec946974584824fd4edd460a8c1a1f48b
Marijn Zwemmer <marijn@marijnzwemmer.com>
2016-01-31 00:03:45 +0100

    Adds all PsMove stuff

---
+++ Assets/PSMove/Code/NotificationManager.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class NotificationManager : MonoBehaviour {
+
+	public static NotificationManager Instance;
+	
+	public GameObject NotificationPrefab;
+
+
+	List<GameObject> messages = new List<GameObject>();
+
+	// Use this for initialization
+	void Awake () {
+		Instance = this;
+	}
+	
+	public void ShowMessage(string message)
+	{
+		var notification = GameObject.Instantiate(NotificationPrefab);
+
+		var text = notification.GetComponent<Text>();
+		text.text = message;
+
+		messages.Add(notification);
+
+		notification.transform.SetParent (GameObject.Find ("Canvas").transform);
+	}
+
+	public void Update(){
+
+		for(int i = messages.Count -1; i >= 0; i--)
+		{
+			var msg = messages[i];
+			var pos = msg.transform.position;
+			pos.y -= 5;
+			msg.transform.position = pos;
+
+			if(pos.y <= -50){
+				GameObject.Destroy(msg.gameObject);
+				messages.RemoveAt(i);
+			}
+		}
+
+
+	}
+}
---
+++ Assets/PSMove/JustAGestureGame.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class JustAGestureGame : MonoBehaviour {
+
+	private static Dictionary<GestureType, Color> GesturesToColorsMap = new Dictionary<GestureType, Color>(){
+		{GestureType.Left, Color.green},
+		{GestureType.Right, Color.blue},
+		{GestureType.Up, Color.red}
+	};
+
+	void Start () 
+	{
+		GestureManager.Instance.OnGesture += OnGestureHandler;
+	}
+
+	void OnGestureHandler(GestureEvent ev)
+	{
+		NotificationManager.Instance.ShowMessage(ev.ControllerId + " : " + ev.GestureType.ToString());
+		GestureManager.Instance.SetControllerLEDColor(ev.ControllerId, GesturesToColorsMap[ev.GestureType]);
+		GestureManager.Instance.SetControllerRumble(ev.ControllerId, 0.3f);
+	}
+}
