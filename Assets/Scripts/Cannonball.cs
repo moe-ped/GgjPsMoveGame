5e531df5d6e31003b2060bfc6449345f786622eb
Mihail Georgiev <mihailbg90@gmail.com>
2016-01-31 00:10:33 +0100

    add died dragon animation and kill it when cannonball hits it

--- Assets/Scripts/Cannonball.cs
+++ Assets/Scripts/Cannonball.cs
@@ -5,8 +5,14 @@ public class Cannonball : MonoBehaviour {
 
 	void OnCollisionEnter2D (Collision2D other) {
 		if (other.gameObject.tag == "Enemy") {
-			Destroy (other.gameObject);
 			Destroy (gameObject);
+			StartCoroutine(DragonDied(other.gameObject));
 		}
+	}
+
+	IEnumerator DragonDied(GameObject dragon) {
+		dragon.GetComponent<Animator>().Play("Died");
+		yield return new WaitForSeconds(3f);
+		Destroy (dragon);
 	}
 }
