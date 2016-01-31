1cdc57c56e7dbc55e3f9f91b3331e9de58706289
Mihail Georgiev <mihailbg90@gmail.com>
2016-01-31 11:06:10 +0100

    Add MainMenu Scene and Health + Level UI

---
+++ Assets/Scripts/HealthManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthManager : MonoBehaviour {
+
+	public static HealthManager Instance;
+
+	[SerializeField]
+	int Lives = 5;
+
+	// Use this for initialization
+	void Awake () {
+		CreateInstance();
+	}
+
+	void CreateInstance() {
+		if(Instance == null) 
+			Instance = this;
+	}
+
+	public void LoseLive() {
+		UpdateLiveUI();
+		Lives--;
+	}
+
+	void UpdateLiveUI() {
+		GameObject[] lives = GameObject.FindGameObjectsWithTag("Live");
+		if(lives.Length > 0) {
+			Destroy(lives[lives.Length-1]);
+		}
+	}
+}
