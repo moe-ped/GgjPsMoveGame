5aa5a761e43ebe71603ed19b115f8e55f26e31fd
moe-ped <m.berning@web.de>
2016-01-31 13:32:53 +0100

    Started implementing effects for visualizing the selected element

---
+++ Assets/Scripts/ApprenticeAura.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ApprenticeAura : MonoBehaviour {
+	
+	[SerializeField]
+	private Transform[] AuraEffects;
+	[SerializeField]
+	private int PlayerId;
+
+	private IGestureProvider GestureProvider;
+
+
+	void Start () {
+		if(Application.platform == RuntimePlatform.OSXEditor){
+			GestureProvider = FindObjectOfType<GestureManager> ();
+		} else{
+			GestureProvider = FindObjectOfType<KeyboardGestureProvider> ();
+		}
+
+		GestureProvider.OnGesture += UpdateAura;
+	}
+
+	// NOTE: not using ev
+	void UpdateAura (PSMoveEvent ev) {
+		int activeAura = (int)Cannon.Instance.CurrentGestures [PlayerId];
+		if (activeAura > 2) {
+			return;
+		}
+		foreach (var aura in AuraEffects) {
+			aura.gameObject.SetActive (false);
+		}
+		AuraEffects [activeAura].gameObject.SetActive (true);
+	}
+}
